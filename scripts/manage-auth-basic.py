2e04700ccbc31b4a0484139819e43ac349227488
Ronan GORAIN <rgorain@jahia.com>
2019-10-04 09:30:21 +0200

    PAAS-403: add auth-basic on haproxy

--- scripts/manage-auth-basic.py
+++ scripts/manage-auth-basic.py
@@ -4,6 +4,7 @@ import fileinput
 import sys
 from binascii import a2b_base64
 import re
+import crypt
 
 file_path = sys.argv[1]
 enable = sys.argv[2] == "true"
@@ -11,6 +12,7 @@ enable = sys.argv[2] == "true"
 if enable:
     new_login = sys.argv[3]
     new_password = a2b_base64(str.encode(sys.argv[4])).decode("utf-8")
+    new_password_hash = crypt.crypt(new_password, crypt.mksalt(crypt.METHOD_SHA512))
 
 with fileinput.FileInput(file_path, inplace=True) as file:
     for line in file:
@@ -21,7 +23,7 @@ with fileinput.FileInput(file_path, inplace=True) as file:
                           line)
             # update login/password
             line = re.sub(r'^(\s*user ).*( password ).*$',
-                          r'\1' + new_login + r'\2' + new_password,
+                          r'\1' + new_login + r'\2' + new_password_hash,
                           line)
         else:
             line = re.sub(r'^(\s*)(#?)(.*)(#HTTP_AUTH_BASIC.*)$',
